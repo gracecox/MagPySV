b2b39c2d6650f87535cac0b75e5afc86abdcf948
gracecox <eegac@leeds.ac.uk>
2016-04-12 19:51:56 +0100

    combined io files and fixed tests

--- docs/conf.py
+++ docs/conf.py
@@ -304,4 +304,4 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'pandas': ('http://pandas.pydata.org/pandas-docs/stable/', None)}
